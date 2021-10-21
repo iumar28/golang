2d71c98a23b22cc9257350c4a90529e3fb3e17c0
iumar28 <66588776+iumar28@users.noreply.github.com>
2021-10-21 15:33:40 +0530

    Create variable_declaration_ways.go

---
+++ variable_declaration_ways.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func  main()  {
+	var way1 string
+	way1="This is the first way to declare the variable"
+	var way2 ="This is the second way to declare the variable"
+	way3:="This is the third way to declare the variable"
+
+	fmt.Println(way1)
+	fmt.Println(way2)
+	fmt.Println(way3)
+
+}
